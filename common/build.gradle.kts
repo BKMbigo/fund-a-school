a3b4659df11ece241e4858ca62ef1ee1288f06f8
bkmbigo <bkmbigo@gmail.com>
2023-06-25 14:54:18 +0300

    add: Added play services auth

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -86,6 +86,8 @@ kotlin {
                 implementation(libs.voyager.navigator)
                 implementation(libs.voyager.transitions)
 
+                implementation(libs.gms.play.services.auth)
+
                 implementation(platform("com.google.firebase:firebase-bom:32.1.0"))
                 implementation(libs.firebase.auth.ktx)
                 implementation(libs.firebase.firestore.ktx)
